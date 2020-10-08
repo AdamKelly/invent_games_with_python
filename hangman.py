4c1135d98bee527c09e9a3d3c1365f8306b7fd32
Adam Kelly <adam.kelly@live.ie>
2020-10-08 20:20:41 +0100

    Work in progress

---
+++ hangman.py
@@ -0,0 +1,53 @@
+import random
+
+# Uppercase letters = constant variable
+# A list is used to hold the various pics the game will use
+HANGMAN_PICS = ['''
+   +---+
+       |
+       |
+       |
+      ===''', '''
+   +---+
+   O   |
+       |
+       |
+      ===''', '''
+   +---+
+   O   |
+   |   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|   |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+       |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  /    |
+      ===''', '''
+   +---+
+   O   |
+  /|\  |
+  / \  |
+      ===''']
+
+words = 'cloud Domain VPN Exploit Beach Firewall Malware Virus Ransomware'.split()
+
+
+def getRandomWord(wordList):
+    # This function returns a random string from the passed list of strings.
+    wordIndex = random.randint(0, len(wordList) - 1)
+    return wordList[wordIndex]
+
+
+print('CYBER HANGMAN')
+#Print the last item by index number from the list
+print(HANGMAN_PICS[6])
